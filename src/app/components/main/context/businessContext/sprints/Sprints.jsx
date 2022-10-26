a11e15a0eb45bdc308a36e68320654e5582fe533
Oleksii Maistriuk <drankel0683@gmail.com>
2022-10-26 16:25:13 +0200

    add styles for biusiness-context-card

--- src/app/components/main/context/businessContext/sprints/Sprints.jsx
+++ src/app/components/main/context/businessContext/sprints/Sprints.jsx
@@ -2,11 +2,24 @@ import { useState } from "react";
 import s from "./Sprints.module.css";
 export const Sprints = () => {
   const [selected, setSelected] = useState(true);
+  const [active, setActive] = useState(false);
 
+  const handleClick = (e) => {
+    setSelected(false);
+    setActive(!active);
+    console.log(e);
+  };
   return (
-    <div className={s.main} onClick={() => setSelected(false)}>
+    <div className={s.main}>
       <ul>
-        <li className={s.card}>
+        <li
+          onClick={(e) => handleClick(e)}
+          className={s.card}
+          style={{
+            boxShadow: `${active ? "0px 8px 16px rgba(84, 84, 84, 0.2)" : ""}`,
+            borderRadius: `${active ? "12px" : ""}`,
+          }}
+        >
           <div className={s.cardTitle}>
             {selected ? (
               <div className={s.new}>
@@ -15,38 +28,6 @@ export const Sprints = () => {
             ) : (
               ""
             )}
-            <span>Olga Nelson</span>
-            <span> • </span>
-            <span>Dec 17</span>
-          </div>
-          <h4>New sprint, tasks and intro information.</h4>
-          <p className={s.text}>
-            Hi Eric, congratulations on completing the previous assignment. This
-            time you will have to focus on...
-          </p>
-        </li>
-      </ul>
-      <ul>
-        <li className={s.card}>
-          <div className={s.cardTitle}>
-            <div className={s.new}>
-              <p>NEW</p>
-            </div>
-            <span>Olga Nelson</span>
-            <span> • </span>
-            <span>Dec 17</span>
-          </div>
-          <h4>New sprint, tasks and intro information.</h4>
-          <p className={s.text}>
-            Hi Eric, congratulations on completing the previous assignment. This
-            time you will have to focus on...
-          </p>
-        </li>
-        <li className={s.card}>
-          <div className={s.cardTitle}>
-            <div className={s.new}>
-              <p>NEW</p>
-            </div>
             <span>Olga Nelson</span>
             <span> • </span>
             <span>Dec 17</span>
